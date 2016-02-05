96a400da040ce8d151f61031c66cc51d5c02c010
Oleg Sulyanov <oleg@sulyanov.com>
2016-02-05 10:34:23 +0700

    Clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
   def valid_password?(password)
     return false if encrypted_password.blank?
     enc = encrypted_password
